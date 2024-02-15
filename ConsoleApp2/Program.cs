0a90f16f6575ce0c82716546eaf221f54ae87d6f
qwerty <qwerty@DESKTOP-Q7D7ISA>
2024-02-15 22:31:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        Console.WriteLine(Math.Cos(x) / (Math.PI - 2 * x) + 16 * x * Math.Cos(x / y) - 2);
+    }
+}
+
